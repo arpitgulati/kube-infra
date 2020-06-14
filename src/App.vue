07a88130bfdd8da3956a3882f8bb5dc8a2bbcd85
Arpit Gulati <arpitgulati@gofynd.com>
2020-06-14 21:52:32 +0530

    testing deployment

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@ export default {
   data() {
     return {
       user: {
-        firstName: 'Harry',
-        lastName: 'Manchanda'
+        firstName: 'Arpit',
+        lastName: 'Gulati'
       }
     };
   },
